67aae8991073a801b579e8c61ba8db1af9916362
Jlesse <jlesseagile@gmail.com>
2015-09-12 15:39:28 -0500

    change website look, add array/hash blog

---
+++ blog/temp.rb
@@ -0,0 +1,9 @@
+
+#create a new hash called breakfastList
+breakfastList = Hash.new
+
+#Put the key "Monday" with value "Eggs!!!!!!" into the hash
+breakfastList["Monday"] = "Eggs!!!!!!"
+
+#Print out the value associated with the key "Monday".
+puts breakfastList["Monday"]
