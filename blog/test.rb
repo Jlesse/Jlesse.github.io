ab8fc6d4f171f5c30165d5f716f137bdc7adb9a0
Jlesse <jlesseagile@gmail.com>
2015-09-19 20:56:07 -0500

    add pen test blog

---
+++ blog/test.rb
@@ -0,0 +1,23 @@
+breakfasts = ["Eggs!", "Eagle Eggs!!", "Pizza", "Potato chips", "Pancakes", "Cereal", "Cookies" ]
+
+#grouped by first letter
+
+first_letter_enum = breakfasts.group_by { |breakfast|
+  breakfast[0]}
+
+puts first_letter_enum
+
+#grouped by length
+
+length_enum = breakfasts.group_by {|breakfast|
+  breakfast.length}
+
+puts length_enum
+
+#grouped by common element "!"
+
+common_element_enum = breakfasts.group_by { |breakfast|
+  breakfast.include? "!"
+}
+
+puts common_element_enum
