021890d37d8ced1da327a75b7dc0cb8639fff6c0
Julian Lesse <jlesseagile@gmail.com>
2015-10-28 14:44:38 -0500

    Update sketch.js

--- p5-release/empty-example/sketch.js
+++ p5-release/empty-example/sketch.js
@@ -20,18 +20,18 @@ console.log(fft.getEnergy("mid"))
   
 if (x1 > 1000) {modifier = -2}
 else if (x1 < 1) {modifier = 2}
-      x1= x1+modifier;
-      y1= y1+modifier;
+      x1= 50; //x1+modifier;
+      y1= 50; //y1+modifier;
 
  rotateX(frameCount * 0.01);
  rotateY(frameCount * 1.00);
 
   box(x1, y1, 200);
 
-  translate(-150,0,0);
-  sphere(50,6);
-  translate(150,0,0);
-  sphere(50,6);
-  torus(50, 8, 5);
+  //translate(-150,0,0);
+  // sphere(50,6);
+  //translate(150,0,0);
+  // sphere(50,6);
+  // torus(50, 8, 5);
 
 }
